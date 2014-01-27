f9e6e5cac97d9fefe84057779f0d54ce888b0876
Chris Fargen <chrisfargen@gmail.com>
2014-01-27 19:57:13 +0000

    Reorganize file structure of v7

--- v7/index.php
+++ v7/index.php
@@ -15,7 +15,7 @@
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
         
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
-        <script src="assets/scripts/jquery-ui-1.10.3.custom.min.js"></script>
+        <script src="assets/scripts/vendor/jquery-ui-1.10.3.custom.min.js"></script>
 
     </head>
     <body>
